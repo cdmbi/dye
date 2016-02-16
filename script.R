b5684bdee556711d304a0dd86bf8cab9d6ab683f
Saw Simeon <doe_doe207@hotmail.com>
2016-02-16 11:03:00 +0700

    12 importance plots added

--- script.R
+++ script.R
@@ -17,7 +17,7 @@ read_file <- function(x){
 
 AtomPairs2D_fingerPrintCount <- read_file("AtomPairs2DFingerprintCount.csv")
 AtomPairs2D_fingerPrinter <- read_file("AtomPairs2DFingerprinter.csv")
-Substructure_fingerPrintCount <- read_file("substructure_fingerprint.csv")
+Substructure_fingerPrintCount <- read_file("substructure_fingerprint_count.csv")
 Substructure_fingerPrinter <- read_file("substructure_fingerprint.csv")
 Extended_finterPrinter <- read_file("extended_finger_printer.csv")
 FingerPrinter <- read_file("finger_printer.csv")
@@ -318,5 +318,134 @@ testing_results_classification <- lapply(input, function(x) {
 })
 
 
+randomForest_feature_importance <- function(x) {
+  library(doSNOW)
+  library(foreach)
+  library(parallel)
+  cl <- makeCluster(8)
+  registerDoSNOW(cl)
+  
+  results <- list(100)
+  results <- foreach (i = 1:100) %dopar% {
+    
+    data <- x
+    trainIndex <- caret::createDataPartition(data$Activity, p = .9,
+                                             list = FALSE, times = 1)
+    train <- data[trainIndex, ]
+    test <- data[-trainIndex, ]
+    model <- randomForest::randomForest(Activity~., data = train, importance = TRUE)
+    #sel <- prospectr::naes(x, k = 90, pc = 5, iter.max = 100)
+    #myData <- x[sel$model, ]
+    #Test <- x[sel$test, ]
+    #ctrl <- caret::trainControl(method = "repeatedcv", number = 10, repeats = 1)
+    #tune <- caret::train(pIC50 ~., data = myData,  method = "rf", tuneLength = 10,
+    #                     trControl = ctrl)
+    #model <- randomForest::randomForest(pIC50~., data = myData, importance = TRUE,
+    #                                    mtry = tune$bestTune[[1]])
+    importance <- model$importance
+    rm(data)
+    rm(trainIndex)
+    rm(trian)
+    rm(test)
+    rm(model)
+    results[[i]] <- importance
+  }
+  return(results)
+  stopCluster(cl)
+}
 
+plot_importance <- function(x) {
+  library(ggplot2)
+  library(cowplot)
+  randomForest_feature_importance_result <- randomForest_feature_importance(x)
+  df <- as.data.frame(randomForest_feature_importance_result)
+  df <- t(apply(df, 1, round, digits = 4))
+  index <- seq(2, 200, by = 2)
+  df <- df[, index]
+  mean <- data.frame(apply(df, 1, mean))
+  sd <- data.frame(apply(df, 1, sd))
+  geni_index <- cbind(mean, sd)
+  colnames(geni_index) <- c("mean", "sd")
+  geni_index <- geni_index[order(mean, decreasing = TRUE),]
+  geni_index <- head(geni_index, 30)
+  set.seed(200)
+  a <- cbind(descriptors = rownames(geni_index), geni_index)
+  a$descriptors <- factor(a$descriptors, levels = a[order(a$mean), "descriptors"])
+  z <- ggplot2::ggplot(a, aes(x = mean, y = descriptors)) +
+    geom_point(size = 4, colour = "black", fill = "red", pch = 21) +
+    geom_errorbarh(aes(xmin = mean-sd, xmax = mean+sd), colour = "black") +
+    ggtitle("") + xlab("Gini index") + ylab ("") +
+    theme(
+      plot.margin = grid::unit(c(1, 1, 1, 1), "cm"),
+      panel.border = element_rect(linetype = "solid", colour = "black", fill = NA, size = 1),
+      axis.title.x = element_text(size = 20, face = "bold", colour = "black"))
+  z + cowplot::background_grid(major = "xy", minor = "none")
+      
+    
+}
+
+save_pdf <- function(x) {
+
+}
+
+pdf("sampleGraph", width = 5, height = 10)
+dev.off()
+
+
+
+a <- plot_importance(AtomPairs2D_fingerPrinter)
+b <- plot_importance(AtomPairs2D_fingerPrintCount)
+c <- plot_importance(Substructure_fingerPrinter)
+d <- plot_importance(Substructure_fingerPrintCount)
+e <- plot_importance(Extended_finterPrinter)
+f <- plot_importance(FingerPrinter) #### re start
+g <- plot_importance(Estate_FingerPrinter)
+h <- plot_importance(GraphOnly_FingerPrinter)
+i <- plot_importance(KlekotaRoth_FingerprintCount)
+j <- plot_importance(KlekotaRoth_FingerPrinter)
+k <- plot_importance(MACCS_FingerPrinter)
+
+
+Extended_finterPrinter <- read_file("extended_finger_printer.csv")
+FingerPrinter <- read_file("finger_printer.csv")
+Estate_FingerPrinter <- read_file("estate_fingerprint.csv")
+GraphOnly_FingerPrinter <- read_file("graph_only_fingerprint.csv")
+KlekotaRoth_FingerprintCount <- read_file("KlekotaRothFingerprintCount.csv")
+KlekotaRoth_FingerPrinter <- read_file("KlekotaRothFingerprinter.csv")
+MACCS_FingerPrinter <- read_file("MACCS_finger_printer.csv")
+Pubchem_FingerPrinter <- read_file("Pubchem_finger_printer.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+geni_index <- geni_index[order(mean, decreasing = TRUE),]
+geni_index <- head(geni_index, 20)
+set.seed(200)
+a <- cbind(descriptors = rownames(geni_index), geni_index)
+a$descriptors <- factor(a$descriptors, levels = a[order(a$mean), "descriptors"])
+### plot
+z <- ggplot(a, aes(x = mean, y = descriptors)) +
+  geom_point(size = 4, colour = "black", fill = "red", pch = 21) +
+  geom_errorbarh(aes(xmin = mean-sd, xmax = mean+sd), colour = "black") +
+  ggtitle("") + xlab("Gini index") + ylab("") +
+  theme(
+    plot.margin = unit(c(1, 1, 1, 1), "cm"),
+    panel.border = element_rect(linetype = "solid", colour = "black", fill = NA, size = 1),
+    axis.title.x = element_text(size = 20, face = "bold", colour = "black"))
+z + background_grid(major = "xy", minor = "none")
 
